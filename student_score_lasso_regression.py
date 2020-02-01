2ef48fc6bc9ec55315338849b921ccda32f7e7fc
dpringlewood <dpringlewood@gmail.com>
2020-02-01 10:43:58 +0400

    Master Branch

---
+++ student_score_lasso_regression.py
@@ -0,0 +1,67 @@
+"""
+Beginners level Machine Learning (ML) project to build my skills.
+The aim here is to predict a students final score (G3) based on a number
+of factors. This will be a regression based ML project using sklearn.
+
+The idea in this is to use a lasso regression analysis. Lasso allows
+the model to normalise the factors, and shrink the factors that are not
+important to 0.
+
+This is important in a professional sense as it allows us to identify
+what effects the predicted value.
+"""
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.linear_model import Lasso
+import matplotlib.pyplot as plt
+
+# Import the data and inspect it
+maths = pd.read_csv(r'./student-mat.csv', sep=';')
+print(maths.head())
+print(maths.info())
+
+"""
+Cleaned the data to only have integer values. I have
+included all integer values in the hope that the Lasso
+algorithm will filter out the ones that are not needed.
+"""
+maths = maths.select_dtypes('int64')
+print(maths.info())
+
+# set our prediction to G3 and split the data
+
+predict = 'G3'
+
+X = np.array(maths.drop(predict, axis=1))
+y = np.array(maths[predict])
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42
+)
+
+# Initialize and train the model
+lasso = Lasso(alpha=0.01, normalize=True)
+lasso.fit(X_train, y_train)
+
+# Find the lasso coefficients
+lasso_coef = lasso.fit(X_train, y_train).coef_
+print(lasso_coef)
+
+# Lets chart these coefficients so that we can use them
+# for explanation purposes.
+maths_cols = maths.columns
+maths_cols = maths_cols[:-1]
+plt.bar(range(len(maths_cols)), lasso_coef)
+plt.xticks(range(len(maths_cols)), maths_cols.values, rotation=60)
+plt.show()
+
+"""
+Predictably the results that students get in previous assessments, 
+and how many failures they have had goes a long way in explaining what 
+they will achieve in their final grade. What is interesting when adjusting 
+the alpha in the lasso regression is some of the other factors that show. 
+The quality of family relationships is a factor in how well a student will
+preform.
+"""
--- student_score_prediction.py
+++ student_score_prediction.py
@@ -6,8 +6,9 @@ of factors. This will be a regression based ML project using sklearn.
 
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import accuracy_score
 
 # Import the data and inspect it
 maths = pd.read_csv(r'./student-mat.csv', sep=';')
@@ -17,12 +18,14 @@ print(maths.info())
 """
 Important to note here that the data is generally very well structured.
 There are no missing values and it is very complete. However sklearn
-can only accept numerical inputs. In this case I will only use what numerical
-columns we have. I will leave the pre-processing of labeled data for a more
-in-depth project.
+can only accept numerical inputs. In this case I will only use some of the 
+numerical columns we have. I will leave the pre-processing of labeled 
+data for a more in-depth project.
 """
 
-maths = maths.select_dtypes(include='int64')
+maths = maths.select_dtypes('int64')
+maths = maths[['studytime', 'failures', 'absences', 'G1', 'G2', 'G3']]
+print(maths.info())
 
 # set our prediction of a students final score (G3)
 predict = 'G3'
@@ -36,11 +39,18 @@ y = np.array(maths[predict])
 # and testing data.
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.3, random_state=42
+    X, y, test_size=0.8, random_state=42
 )
 
 # Now we need to initiate our model and train it
 
 linear = LinearRegression()
+# Cross-Valadation testing
+
+cv_scores = cross_val_score(linear, X, y, cv=5)
+print('The CV R^2 are: ', cv_scores)
+print('The mean R^2 from CV is: ', np.mean(cv_scores))
 linear.fit(X_train, y_train)
-print(linear.score(X_test, y_test))
+print("The R^2 is: ", linear.score(X_test, y_test))
+
+
